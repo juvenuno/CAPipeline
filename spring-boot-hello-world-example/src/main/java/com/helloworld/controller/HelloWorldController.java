154b4cb9ada8ee7eacc5ac2eda9bc037ab0288e7
juvenuno <nunojscorreia@hotmail.com>
2022-05-20 14:28:14 +0100

    revert back java file

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> This a new piece on info was added via PipelineCA. To prove it's working - NUNO CORREIA - Test </h1>" ;
+return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application.</h1>" ;
 }
 }
