e0d0a1b156b43bff4a75c6062a008bf149f0ec77
juvenuno <nunojscorreia@hotmail.com>
2022-05-20 12:06:59 +0100

    2nd try

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,7 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>"
-	"<h1> This piece on info was added via PipelineCA. To prove it's working - NUNO CORREIA </h1>";
+return "<h1> This piece on info was added via PipelineCA. To prove it's working - NUNO CORREIA </h1>" ;
 }
 }
