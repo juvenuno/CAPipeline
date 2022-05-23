5ea5b105c70cd4c14698ac4db0a63532be2a8907
Gary Clarke <gclarke2801@gmail.com>
2022-05-23 22:09:37 +0100

    gary's first commit

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1>Nuno change Locally and was automatically deploy to AWS! Colleen added this part!!</h1>" ;
+return "<h1>Nuno change Locally and was automatically deploy to AWS! Colleen added this part!! and now gary added this part!!</h1>" ;
 }
 }
