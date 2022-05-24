a2e7c2c401aa6709d8db4a4fc69e34d6e2ab2209
Colleen Lynch <10503976@mydbs.ie>
2022-05-24 21:33:00 +0100

    Colleens update

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1>Nuno change Locally and was automatically deploy to AWS! Colleen added this part!! and now gary added this part!!, we performed another update! Colleen performed another update here ! nuno is a legend</h1>";
+return "<h1>Nuno change Locally and was automatically deploy to AWS! Colleen added this part!! and now gary added this part!!, we performed another update! Colleen performed another update here ! nuno is a legend - Today is a lovely day - CL </h1>";
 }
 }
