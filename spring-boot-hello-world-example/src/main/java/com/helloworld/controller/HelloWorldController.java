b20500fdfd1f0cf40f7585e274f0f696310907ad
juvenuno <nunojscorreia@hotmail.com>
2022-05-26 20:01:41 +0100

    This a DBS TEST

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1>I thought i was special too!! I was so wrong!! </h1>";
+return "<h1>This is a test! </h1>";
 }
 }
